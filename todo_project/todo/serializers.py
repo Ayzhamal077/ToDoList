4b5d1c217615416d93c4d1fec98f2bb18f147980
Guest's login <Ayzhamal077@gmail.com>
2025-02-01 16:13:41 +0400

    Fix serializers and views

--- todo_project/todo/serializers.py
+++ todo_project/todo/serializers.py
@@ -5,3 +5,4 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ["id", "title", "description","completed", "created"]
+        read_only_fields = ["id", "created"]
